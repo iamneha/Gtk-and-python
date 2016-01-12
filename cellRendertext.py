c87daac23cb74a37a4b772e2e1e7c3448d89ed3b
iamneha <inehayadav28@gmail.com>
2016-01-13 00:17:16 +0530

    CellRenderedtext

--- cellRendertext.py
+++ cellRendertext.py
@@ -1,14 +1,33 @@
 from gi.repository import Gtk
-class CellRenderText(Gtk.Window)
+class CellRendererText(Gtk.Window):
        def __init__(self):
 	Gtk.Window.__init__(self, title="Text")
 	self.set_default_size(200, 200)	
 
 	self.liststore = Gtk.ListStore(str, str)
 	self.liststore.append(["Fedora","http://fedoraproject.org/" ])
-	self.liststore.append(["Fedora","http://fedoraproject.org/" ])
+	self.liststore.append(["Facebook","https://web.facebook.com/profile.php?id=100001996874688" ])
+	self.liststore.append(["Github","https://github.com/iamneha"])
 
 	treeview = Gtk.TreeView(model=self.liststore)
 
 	renderer_text = Gtk.CellRendererText()
-	column_text = Gtk.TreeViewColumn("Text,")
+	column_text = Gtk.TreeViewColumn("Text", renderer_text, text=0)
+	treeview.append_column(column_text)
+
+	renderer_editabletext = Gtk.CellRendererText()
+	renderer_editabletext.set_property("editable", True)
+
+	column_editabletext = Gtk.TreeViewColumn("Editable text", renderer_editabletext, text=1)
+	treeview.append_column(column_editabletext)
+	
+	renderer_editabletext.connect("edited", self.text_edited)
+
+	self.add(treeview)
+
+       def text_edited(self, widget, path, text):
+	self.liststore[path][1] = text
+win = CellRendererText()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
